7cf150aa5c3a54d26c73a5a861c4df2d3b6783f3
Rémi Barbier <remi.barbier@hec.edu>
2025-04-19 19:54:04 +0200

    Add minimal emoji_picker_flutter_custom wrapper with JVM 1.8 compatibility

--- emoji_picker_flutter_custom/lib/emoji_picker_flutter.dart
+++ emoji_picker_flutter_custom/lib/emoji_picker_flutter.dart
@@ -1,16 +1,6 @@
+// Cette bibliothèque réexporte simplement le package emoji_picker_flutter original
+// mais avec des modifications de build pour assurer la compatibilité JVM 1.8
+
 library emoji_picker_flutter;
 
-export 'package:emoji_picker_flutter/src/category_emoji.dart';
-export 'package:emoji_picker_flutter/src/category_icons.dart';
-export 'package:emoji_picker_flutter/src/config.dart';
-export 'package:emoji_picker_flutter/src/default_emoji_set.dart';
-export 'package:emoji_picker_flutter/src/emoji.dart';
-export 'package:emoji_picker_flutter/src/emoji_cell.dart';
-export 'package:emoji_picker_flutter/src/emoji_container.dart';
-export 'package:emoji_picker_flutter/src/emoji_picker.dart';
-export 'package:emoji_picker_flutter/src/emoji_picker_builder.dart';
-export 'package:emoji_picker_flutter/src/emoji_picker_utils.dart';
-export 'package:emoji_picker_flutter/src/emoji_skin_tones.dart';
-export 'package:emoji_picker_flutter/src/emoji_text_editing_controller.dart';
-export 'package:emoji_picker_flutter/src/emoji_view_state.dart';
-export 'package:emoji_picker_flutter/src/recent_tab_behavior.dart';
+export 'package:emoji_picker_flutter/emoji_picker_flutter.dart'; 
